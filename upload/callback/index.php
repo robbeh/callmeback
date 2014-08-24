e283912f8d615a3f73bd6178ca807c5d92c2774d
Rob .T <robbeh@github.com>
2014-08-24 12:52:51 +0100

    Remove old tests
    Signed-off-by: Rob .T <robbeh@github.com>

--- upload/callback/index.php
+++ upload/callback/index.php
@@ -16,17 +16,6 @@
 	/** Load the configuration file **/
 	$configuration = require dirname(__FILE__) . '/config.php';
 
-	/** Inject some test data **/
-//	Callback_Security::generateCaptchaQuestion();
-//	$_POST['name'] = 'rob';
-//	$_POST['number'] = '12345';
-//	$_POST['time'] = '    3434';
-//	$_POST['bot_protection'] = '';
-//	$_POST['csrf'] = Callback_Security::generateCsrf();
-//	$_POST['captcha'] = Callback_Security::getCaptchaAnswer();
-//	$_POST['captcha'] = '2';
-
-
 	/** Check that the form posted is ok **/
 	$form = new Form($configuration);
 	$form->validate($_POST);
